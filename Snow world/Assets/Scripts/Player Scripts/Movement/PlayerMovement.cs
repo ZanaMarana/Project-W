acda1407824212813c9345609519564da4eabecc
Ailius <translucentailius@gmail.com>
2024-03-13 02:07:45 -0400

    Update PlayerMovement.cs

--- Snow world/Assets/Scripts/Player Scripts/Movement/PlayerMovement.cs
+++ Snow world/Assets/Scripts/Player Scripts/Movement/PlayerMovement.cs
@@ -8,6 +8,13 @@ public class PlayerMovement : MonoBehaviour
     [Header("Movement")]
     public float moveSpeed = 1f;
 
+    public float groundDrag;
+
+    [Header("Ground Check")]
+    public float playerHeight;
+    public LayerMask whatIsGround;
+    bool grounded;
+
     public Transform orientation;
 
     Vector3 moveDir;
@@ -15,6 +22,24 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody rb;
 
     public InputActionReference move;
+
+    public Collider groundCheck;
+
+    private void Update()
+    {
+        grounded = groundCheck.
+
+        if (grounded)
+        {
+            rb.drag = groundDrag;
+        }
+        else
+        {
+            rb.drag = 0;
+        }
+
+        speedControl();
+    }
 
     // Update is called once per frame
     private void FixedUpdate()
@@ -28,5 +53,16 @@ public class PlayerMovement : MonoBehaviour
         moveDir = orientation.forward * moveValue.y + orientation.right * moveValue.x;
 
         rb.AddForce(moveDir.normalized * moveSpeed * 10f, ForceMode.Force);
+    }
+
+    private void speedControl()
+    {
+        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        if(flatVel.magnitude > moveSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+        }
     }
 }
